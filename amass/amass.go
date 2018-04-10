78f12daa40a1a8e12f31679243de8d8c5f6a94b2
caffix <caffix@gmail.com>
2018-04-10 17:31:40 -0400

    small fix needed for release of version 1.4.0

--- amass/amass.go
+++ amass/amass.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	Version string = "v1.3.2"
+	Version string = "v1.4.0"
 	Author  string = "Jeff Foley (@jeff_foley)"
 	// Tags used to mark the data source with the Subdomain struct
 	ALT     = "alt"
