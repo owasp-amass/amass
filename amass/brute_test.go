531f797b373244de8173f1dc398976b7e49c45c7
Architekton <architekton350@gmail.com>
2019-02-20 16:46:27 +1100

    brute tests
    Update the brute test to reflect changes in the now resolving brute
    service

--- amass/brute_test.go
+++ amass/brute_test.go
@@ -1,43 +1,39 @@
 // Copyright 2017 Jeff Foley. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+// +build brute
+
 package amass
 
 import (
+	"log"
+	"strings"
+	"testing"
+	"time"
+
 	"github.com/OWASP/Amass/amass/core"
 	"github.com/miekg/dns"
 )
 
 var (
-	domains  = []string{"claritysec.com", "twitter.com", "google.com", "github.com"}
-	wordlist = []string{"foo", "bar"}
+	domains  = []string{"google.com"}
+	wordlist = []string{"images", "search", "mail"}
 
 	// Resolved bruteTestRequests
 	bruteTestRequests = []*core.Request{
 		&core.Request{
-			Name:    "test.claritysec.com",
-			Domain:  "claritysec.com",
+			Name:    "bing.com",
+			Domain:  "bing.com",
 			Records: []core.DNSAnswer{core.DNSAnswer{Type: int(dns.TypeA)}},
 		},
 		&core.Request{
-			Name:    "test.twitter.com",
-			Domain:  "twitter.com",
-			Records: []core.DNSAnswer{core.DNSAnswer{Type: int(dns.TypeA)}},
-		},
-		&core.Request{
-			Name:    "test.google.com",
-			Domain:  "google.com",
-			Records: []core.DNSAnswer{core.DNSAnswer{Type: int(dns.TypeA)}},
-		},
-		&core.Request{
-			Name:    "test.github.com",
-			Domain:  "github.com",
+			Name:    "yahoo.com",
+			Domain:  "yahoo.com",
 			Records: []core.DNSAnswer{core.DNSAnswer{Type: int(dns.TypeA)}},
 		},
 	}
 )
 
-/*
 func TestBruteForceRootDomains(t *testing.T) {
 	config := &core.Config{}
 	config.Wordlist = wordlist
@@ -49,7 +45,7 @@ func TestBruteForceRootDomains(t *testing.T) {
 
 	out := make(chan *core.Request)
 	bus := core.NewEventBus()
-	bus.Subscribe(core.NewNameTopic, func(req *core.Request) {
+	bus.Subscribe(core.NameResolvedTopic, func(req *core.Request) {
 		out <- req
 	})
 	defer bus.Stop()
@@ -58,22 +54,22 @@ func TestBruteForceRootDomains(t *testing.T) {
 	srv.Start()
 	defer srv.Stop()
 
+	count := 0
 	expected := len(config.Wordlist) * len(domains)
-	results := make(map[string]int)
-	done := time.After(time.Second)
+	done := time.After(time.Second * 10)
 
 loop:
 	for {
 		select {
-		case req := <-out:
-			results[req.Name]++
+		case <-out:
+			count++
 		case <-done:
 			break loop
 		}
 	}
 
-	if expected != len(results) {
-		t.Errorf("Got %d names, expected %d instead", len(results), expected)
+	if expected != count {
+		t.Errorf("Got %d names, expected %d instead", count, expected)
 	}
 }
 
@@ -90,7 +86,7 @@ func TestBruteForceMinForRecursive(t *testing.T) {
 
 	out := make(chan *core.Request)
 	bus := core.NewEventBus()
-	bus.Subscribe(core.NewNameTopic, func(req *core.Request) {
+	bus.Subscribe(core.NameResolvedTopic, func(req *core.Request) {
 		out <- req
 	})
 	defer bus.Stop()
@@ -104,25 +100,22 @@ func TestBruteForceMinForRecursive(t *testing.T) {
 
 	// Should pass
 	bus.Publish(core.NewSubdomainTopic, bruteTestRequests[1], 2)
-	bus.Publish(core.NewSubdomainTopic, bruteTestRequests[2], 3)
-	bus.Publish(core.NewSubdomainTopic, bruteTestRequests[3], 4)
 
 	expected := len(config.Wordlist) * (len(bruteTestRequests) - 1 + len(domains))
-	results := make(map[string]int)
-	done := time.After(time.Second)
+	count := 0
+	done := time.After(time.Second * 15)
 
 loop:
 	for {
 		select {
-		case req := <-out:
-			results[req.Name]++
+		case <-out:
+			count++
 		case <-done:
 			break loop
 		}
 	}
 
-	if expected != len(results) {
-		t.Errorf("Got %d names, expected %d instead", len(results), expected)
+	if expected != count {
+		t.Errorf("Got %d names, expected %d instead", count, expected)
 	}
 }
-*/
