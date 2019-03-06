d8ef548ff1f3eed554ecbd3b514650ce0539920d
caffix <caffix@gmail.com>
2019-03-05 19:18:29 -0500

    tracker can use multiple domain names

--- cmd/amass.tracker/main.go
+++ cmd/amass.tracker/main.go
@@ -37,13 +37,14 @@ var (
 	green  = color.New(color.FgHiGreen).SprintFunc()
 	blue   = color.New(color.FgHiBlue).SprintFunc()
 	// Command-line switches and provided parameters
-	help     = flag.Bool("h", false, "Show the program usage message")
-	list     = flag.Bool("list", false, "Print information for all available enumerations")
-	vprint   = flag.Bool("version", false, "Print the version number of this Amass binary")
-	dir      = flag.String("dir", "", "Path to the directory containing the graph database")
-	last     = flag.Int("last", 0, "The number of recent enumerations to include in the tracking")
-	startStr = flag.String("start", "", "Exclude all enumerations before (format: "+timeFormat+")")
-	history  = flag.Bool("history", false, "Show the difference between all enumeration pairs")
+	help        = flag.Bool("h", false, "Show the program usage message")
+	list        = flag.Bool("list", false, "Print information for all available enumerations")
+	vprint      = flag.Bool("version", false, "Print the version number of this Amass binary")
+	dir         = flag.String("dir", "", "Path to the directory containing the graph database")
+	last        = flag.Int("last", 0, "The number of recent enumerations to include in the tracking")
+	startStr    = flag.String("start", "", "Exclude all enumerations before (format: "+timeFormat+")")
+	history     = flag.Bool("history", false, "Show the difference between all enumeration pairs")
+	domainspath = flag.String("df", "", "Path to a file providing root domain names")
 )
 
 func main() {
@@ -69,16 +70,24 @@ func main() {
 		fmt.Fprintf(color.Error, "version %s\n", amass.Version)
 		os.Exit(1)
 	}
-	if len(domains) == 0 {
-		r.Fprintln(color.Error, "No root domain names were provided")
-		os.Exit(1)
-	}
 	if *startStr != "" && *last != 0 {
 		r.Fprintln(color.Error, "The start flag cannot be used with the last or all flags")
 		os.Exit(1)
 	}
 	if *last == 1 {
 		r.Fprintln(color.Error, "Tracking requires more than one enumeration")
+		os.Exit(1)
+	}
+	if *domainspath != "" {
+		list, err := core.GetListFromFile(*domainspath)
+		if err != nil {
+			r.Fprintf(color.Error, "Failed to parse the domain names file: %v\n", err)
+			os.Exit(1)
+		}
+		domains = utils.UniqueAppend(domains, list...)
+	}
+	if len(domains) == 0 {
+		r.Fprintln(color.Error, "No root domain names were provided")
 		os.Exit(1)
 	}
 
@@ -113,8 +122,11 @@ func main() {
 	var enums []string
 	// Obtain the enumerations that include the provided domain
 	for _, e := range graph.EnumerationList() {
-		if enumContainsDomain(e, domains[0], graph) {
-			enums = append(enums, e)
+		for _, domain := range domains {
+			if enumContainsDomain(e, domain, graph) {
+				enums = append(enums, e)
+				break
+			}
 		}
 	}
 
@@ -159,19 +171,19 @@ func main() {
 	}
 
 	if *history {
-		completeHistoryOutput(domains[0], enums, earliest, latest, graph)
+		completeHistoryOutput(domains, enums, earliest, latest, graph)
 		return
 	}
-	cumulativeOutput(domains[0], enums, earliest, latest, graph)
+	cumulativeOutput(domains, enums, earliest, latest, graph)
 }
 
-func cumulativeOutput(domain string, enums []string, ea, la []time.Time, h handlers.DataHandler) {
+func cumulativeOutput(domains []string, enums []string, ea, la []time.Time, h handlers.DataHandler) {
 	idx := len(enums) - 1
 	filter := utils.NewStringFilter()
 
 	var cum []*core.Output
 	for i := idx - 1; i >= 0; i-- {
-		for _, out := range getEnumDataInScope(domain, enums[i], h) {
+		for _, out := range getEnumDataInScope(domains, enums[i], h) {
 			if !filter.Duplicate(out.Name) {
 				cum = append(cum, out)
 			}
@@ -185,8 +197,8 @@ func cumulativeOutput(domain string, enums []string, ea, la []time.Time, h handl
 	blueLine()
 
 	var updates bool
-	out := getEnumDataInScope(domain, enums[idx], h)
-	for _, d := range diffEnumOutput(domain, cum, out) {
+	out := getEnumDataInScope(domains, enums[idx], h)
+	for _, d := range diffEnumOutput(cum, out) {
 		updates = true
 		fmt.Fprintln(color.Output, d)
 	}
@@ -195,7 +207,7 @@ func cumulativeOutput(domain string, enums []string, ea, la []time.Time, h handl
 	}
 }
 
-func completeHistoryOutput(domain string, enums []string, ea, la []time.Time, h handlers.DataHandler) {
+func completeHistoryOutput(domains []string, enums []string, ea, la []time.Time, h handlers.DataHandler) {
 	var prev string
 
 	for i, enum := range enums {
@@ -214,9 +226,9 @@ func completeHistoryOutput(domain string, enums []string, ea, la []time.Time, h 
 		blueLine()
 
 		var updates bool
-		out1 := getEnumDataInScope(domain, prev, h)
-		out2 := getEnumDataInScope(domain, enum, h)
-		for _, d := range diffEnumOutput(domain, out1, out2) {
+		out1 := getEnumDataInScope(domains, prev, h)
+		out2 := getEnumDataInScope(domains, enum, h)
+		for _, d := range diffEnumOutput(out1, out2) {
 			updates = true
 			fmt.Fprintln(color.Output, d)
 		}
@@ -234,40 +246,43 @@ func blueLine() {
 	fmt.Println()
 }
 
-func getEnumDataInScope(domain, enum string, h handlers.DataHandler) []*core.Output {
+func getEnumDataInScope(domains []string, enum string, h handlers.DataHandler) []*core.Output {
 	var out []*core.Output
 
 	for _, o := range h.GetOutput(enum, true) {
-		if strings.HasSuffix(o.Name, domain) {
-			out = append(out, o)
+		for _, domain := range domains {
+			if strings.HasSuffix(o.Name, domain) {
+				out = append(out, o)
+				break
+			}
 		}
 	}
 	return out
 }
 
-func diffEnumOutput(domain string, eout1, eout2 []*core.Output) []string {
-	emap1 := make(map[string]*core.Output)
-	emap2 := make(map[string]*core.Output)
+func diffEnumOutput(out1, out2 []*core.Output) []string {
+	omap1 := make(map[string]*core.Output)
+	omap2 := make(map[string]*core.Output)
 
-	for _, o := range eout1 {
-		emap1[o.Name] = o
+	for _, o := range out1 {
+		omap1[o.Name] = o
 	}
-	for _, o := range eout2 {
-		emap2[o.Name] = o
+	for _, o := range out2 {
+		omap2[o.Name] = o
 	}
 
 	handled := make(map[string]struct{})
 	var diff []string
-	for _, o := range eout1 {
+	for _, o := range out1 {
 		handled[o.Name] = struct{}{}
 
-		if _, found := emap2[o.Name]; !found {
+		if _, found := omap2[o.Name]; !found {
 			diff = append(diff, fmt.Sprintf("%s%s %s", blue("Removed: "),
 				green(o.Name), yellow(lineOfAddresses(o.Addresses))))
 			continue
 		}
 
-		o2 := emap2[o.Name]
+		o2 := omap2[o.Name]
 		if !compareAddresses(o.Addresses, o2.Addresses) {
 			diff = append(diff, fmt.Sprintf("%s%s\n\t%s\t%s\n\t%s\t%s", blue("Moved: "),
 				green(o.Name), blue(" from "), yellow(lineOfAddresses(o.Addresses)),
@@ -275,12 +290,12 @@ func diffEnumOutput(domain string, eout1, eout2 []*core.Output) []string {
 		}
 	}
 
-	for _, o := range eout2 {
+	for _, o := range out2 {
 		if _, found := handled[o.Name]; found {
 			continue
 		}
 
-		if _, found := emap1[o.Name]; !found {
+		if _, found := omap1[o.Name]; !found {
 			diff = append(diff, fmt.Sprintf("%s%s %s", blue("Found: "),
 				green(o.Name), yellow(lineOfAddresses(o.Addresses))))
 		}
