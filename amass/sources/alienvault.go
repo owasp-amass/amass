5b3490bda89802160240e9196d358604286db641
caffix <caffix@gmail.com>
2019-05-18 16:30:45 -0400

    improvement to the AlienVault implementation

--- amass/sources/alienvault.go
+++ amass/sources/alienvault.go
@@ -6,6 +6,8 @@ package sources
 import (
 	"encoding/json"
 	"fmt"
+	"math"
+	"strconv"
 	"strings"
 	"time"
 
@@ -28,7 +30,7 @@ type AlienVault struct {
 func NewAlienVault(config *core.Config, bus *core.EventBus) *AlienVault {
 	a := &AlienVault{
 		SourceType: core.API,
-		RateLimit:  3 * time.Second,
+		RateLimit:  100 * time.Millisecond,
 		haveAPIKey: true,
 	}
 
@@ -71,15 +73,15 @@ func (a *AlienVault) processRequests() {
 }
 
 func (a *AlienVault) executeQuery(domain string) {
-	url := a.getURL(domain)
+	a.SetActive()
+	u := a.getURL(domain) + "passive_dns"
 	headers := map[string]string{"Content-Type": "application/json", "X-OTX-API-KEY": a.API.Key}
-	page, err := utils.RequestWebPage(url, nil, headers, "", "")
+	page, err := utils.RequestWebPage(u, nil, headers, "", "")
 	if err != nil {
-		a.Config().Log.Printf("%s: %s: %v", a.String(), url, err)
+		a.Config().Log.Printf("%s: %s: %v", a.String(), u, err)
 		return
 	}
-
-	// Extract the subdomain names and IP addresses from the results
+	// Extract the subdomain names and IP addresses from the passive DNS information
 	var m struct {
 		Subdomains []struct {
 			Hostname string `json:"hostname"`
@@ -90,35 +92,111 @@ func (a *AlienVault) executeQuery(domain string) {
 		return
 	}
 
-	if len(m.Subdomains) == 0 {
+	var names []string
+	var ips []string
+	re := a.Config().DomainRegex(domain)
+	if len(m.Subdomains) != 0 {
+		for _, sub := range m.Subdomains {
+			n := strings.ToLower(sub.Hostname)
+
+			if re.MatchString(n) {
+				names = append(names, n)
+				ips = append(ips, sub.IP)
+			}
+		}
+	}
+
+	time.Sleep(a.RateLimit)
+	u = a.getURL(domain) + "url_list"
+	page, err = utils.RequestWebPage(u, nil, headers, "", "")
+	if err != nil {
+		a.Config().Log.Printf("%s: %s: %v", a.String(), u, err)
+		return
+	}
+	// Extract the subdomain names and IP addresses from the URL information
+	var urls struct {
+		PageNum  int  `json:"page_num"`
+		HasNext  bool `json:"has_next"`
+		Limit    int  `json:"limit"`
+		FullSize int  `json:"full_size"`
+		URLs     []struct {
+			Domain   string `json:"domain"`
+			Hostname string `json:"hostname"`
+			Result   struct {
+				Worker struct {
+					IP string `json:"ip"`
+				} `json:"urlworker"`
+			} `json:"result"`
+		} `json:"url_list"`
+	}
+	if err := json.Unmarshal([]byte(page), &urls); err != nil {
 		return
 	}
 
-	a.SetActive()
-	re := a.Config().DomainRegex(domain)
-	for _, sub := range m.Subdomains {
-		name := strings.ToLower(sub.Hostname)
+	if len(urls.URLs) != 0 {
+		for _, u := range urls.URLs {
+			n := strings.ToLower(u.Hostname)
 
-		if re.MatchString(name) {
-			a.Bus().Publish(core.NewNameTopic, &core.Request{
-				Name:   name,
-				Domain: domain,
-				Tag:    a.SourceType,
-				Source: a.String(),
-			})
-
-			a.Bus().Publish(core.NewAddrTopic, &core.Request{
-				Address: sub.IP,
-				Domain:  domain,
-				Tag:     a.SourceType,
-				Source:  a.String(),
-			})
+			if re.MatchString(n) {
+				names = utils.UniqueAppend(names, n)
+				if u.Result.Worker.IP != "" {
+					ips = utils.UniqueAppend(ips, u.Result.Worker.IP)
+				}
+			}
 		}
+	}
+	// If there are additional pages of URLs, obtain that info as well
+	if urls.HasNext {
+		pages := int(math.Ceil(float64(urls.FullSize) / float64(urls.Limit)))
+		for cur := urls.PageNum + 1; cur <= pages; cur++ {
+			time.Sleep(a.RateLimit)
+			pageURL := u + "?page=" + strconv.Itoa(cur)
+			page, err = utils.RequestWebPage(pageURL, nil, headers, "", "")
+			if err != nil {
+				a.Config().Log.Printf("%s: %s: %v", a.String(), pageURL, err)
+				break
+			}
+
+			if err := json.Unmarshal([]byte(page), &urls); err != nil {
+				break
+			}
+
+			if len(urls.URLs) != 0 {
+				for _, u := range urls.URLs {
+					n := strings.ToLower(u.Hostname)
+
+					if re.MatchString(n) {
+						names = utils.UniqueAppend(names, n)
+						if u.Result.Worker.IP != "" {
+							ips = utils.UniqueAppend(ips, u.Result.Worker.IP)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for _, name := range names {
+		a.Bus().Publish(core.NewNameTopic, &core.Request{
+			Name:   name,
+			Domain: domain,
+			Tag:    a.SourceType,
+			Source: a.String(),
+		})
+	}
+
+	for _, ip := range ips {
+		a.Bus().Publish(core.NewAddrTopic, &core.Request{
+			Address: ip,
+			Domain:  domain,
+			Tag:     a.SourceType,
+			Source:  a.String(),
+		})
 	}
 }
 
 func (a *AlienVault) getURL(domain string) string {
-	format := "https://otx.alienvault.com/api/v1/indicators/domain/%s/passive_dns"
+	format := "https://otx.alienvault.com/api/v1/indicators/domain/%s/"
 
 	return fmt.Sprintf(format, domain)
 }
