89a9bd67e0783046c80d9e38f9464b770642e520
caffix <caffix@gmail.com>
2019-06-01 16:35:09 -0400

    version 3.0.2 release

--- amass/amass.go
+++ amass/amass.go
@@ -54,7 +54,7 @@ var Banner = `
 
 const (
 	// Version is used to display the current version of Amass.
-	Version = "3.0.1"
+	Version = "3.0.2"
 
 	// Author is used to display the founder of the amass package.
 	Author = "OWASP Amass Project - @owaspamass"
