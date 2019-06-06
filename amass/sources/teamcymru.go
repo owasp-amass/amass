88cacb697bdd11f7313f5d403113e54e9de2a245
caffix <caffix@gmail.com>
2019-06-06 19:54:47 -0400

    added ShadowServer/TeamCymru/ViewDNS data sources

---
+++ amass/sources/teamcymru.go
@@ -0,0 +1,171 @@
+// Copyright 2017 Jeff Foley. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package sources
+
+import (
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/OWASP/Amass/amass/core"
+	"github.com/OWASP/Amass/amass/utils"
+)
+
+// TeamCymru is the Service that handles access to the TeamCymru data source.
+type TeamCymru struct {
+	core.BaseService
+
+	SourceType string
+	RateLimit  time.Duration
+}
+
+// NewTeamCymru returns he object initialized, but not yet started.
+func NewTeamCymru(config *core.Config, bus *core.EventBus) *TeamCymru {
+	t := &TeamCymru{
+		SourceType: core.API,
+		RateLimit:  2 * time.Second,
+	}
+
+	t.BaseService = *core.NewBaseService(t, "TeamCymru", config, bus)
+	return t
+}
+
+// OnStart implements the Service interface
+func (t *TeamCymru) OnStart() error {
+	t.BaseService.OnStart()
+
+	t.Bus().Subscribe(core.IPToASNTopic, t.SendASNRequest)
+	go t.processRequests()
+	return nil
+}
+
+func (t *TeamCymru) processRequests() {
+	last := time.Now().Truncate(10 * time.Minute)
+
+	for {
+		select {
+		case <-t.Quit():
+			return
+		case req := <-t.ASNRequestChan():
+			if time.Now().Sub(last) < t.RateLimit {
+				time.Sleep(t.RateLimit)
+			}
+			last = time.Now()
+			t.executeQuery(req.Address)
+			last = time.Now()
+		case <-t.DNSRequestChan():
+		case <-t.AddrRequestChan():
+		case <-t.WhoisRequestChan():
+		}
+	}
+}
+
+func (t *TeamCymru) executeQuery(addr string) {
+	if addr == "" {
+		return
+	}
+
+	t.SetActive()
+	req := t.origin(addr)
+	if req == nil {
+		return
+	}
+
+	asn := t.asnLookup(req.ASN)
+	if asn == nil {
+		return
+	}
+
+	req.AllocationDate = asn.AllocationDate
+	req.Description = asn.Description
+	t.Bus().Publish(core.NewASNTopic, req)
+}
+
+func (t *TeamCymru) origin(addr string) *core.ASNRequest {
+	var err error
+	var name string
+	var answers []core.DNSAnswer
+	if ip := net.ParseIP(addr); utils.IsIPv4(ip) {
+		name = utils.ReverseIP(addr) + ".origin.asn.cymru.com"
+	} else if utils.IsIPv6(ip) {
+		name = utils.IPv6NibbleFormat(utils.HexString(ip)) + ".origin6.asn.cymru.com"
+	} else {
+		t.Config().Log.Printf("%s: %s: Failed to parse the IP address", t.String(), addr)
+		return nil
+	}
+
+	answers, err = core.Resolve(name, "TXT", core.PriorityHigh)
+	if err != nil {
+		t.Config().Log.Printf("%s: %s: DNS TXT record query error: %v", t.String(), name, err)
+		return nil
+	}
+
+	fields := strings.Split(answers[0].Data, " | ")
+	if len(fields) < 5 {
+		t.Config().Log.Printf("%s: %s: Failed to parse the origin response", t.String(), name)
+		return nil
+	}
+
+	asn, err := strconv.Atoi(strings.TrimSpace(fields[0]))
+	if err != nil {
+		t.Config().Log.Printf("%s: %s: Failed to parse the origin response: %v", t.String(), name, err)
+		return nil
+	}
+
+	at, err := time.Parse("2006-Jan-02", strings.TrimSpace(fields[4]))
+	if err != nil {
+		at = time.Now()
+	}
+
+	return &core.ASNRequest{
+		ASN:            asn,
+		Prefix:         strings.TrimSpace(fields[1]),
+		CC:             strings.TrimSpace(fields[2]),
+		Registry:       strings.TrimSpace(fields[3]),
+		AllocationDate: at,
+		Netblocks:      []string{strings.TrimSpace(fields[1])},
+		Tag:            t.SourceType,
+		Source:         t.String(),
+	}
+}
+
+func (t *TeamCymru) asnLookup(asn int) *core.ASNRequest {
+	var err error
+	var answers []core.DNSAnswer
+	name := "AS" + strconv.Itoa(asn) + ".asn.cymru.com"
+
+	answers, err = core.Resolve(name, "TXT", core.PriorityHigh)
+	if err != nil {
+		t.Config().Log.Printf("%s: %s: DNS TXT record query error: %v", t.String(), name, err)
+		return nil
+	}
+
+	fields := strings.Split(answers[0].Data, " | ")
+	if len(fields) < 5 {
+		t.Config().Log.Printf("%s: %s: Failed to parse the origin response", t.String(), name)
+		return nil
+	}
+
+	pASN, err := strconv.Atoi(strings.TrimSpace(fields[0]))
+	if err != nil || asn != pASN {
+		t.Config().Log.Printf("%s: %s: Failed to parse the origin response: %v", t.String(), name, err)
+		return nil
+	}
+
+	at, err := time.Parse("2006-Jan-02", strings.TrimSpace(fields[3]))
+	if err != nil {
+		at = time.Now()
+	}
+
+	return &core.ASNRequest{
+		ASN:            asn,
+		CC:             strings.TrimSpace(fields[1]),
+		Registry:       strings.TrimSpace(fields[2]),
+		AllocationDate: at,
+		Description:    strings.TrimSpace(fields[4]),
+		Tag:            t.SourceType,
+		Source:         t.String(),
+	}
+}
---
+++ amass/sources/viewdns.go
@@ -0,0 +1,158 @@
+// Copyright 2017 Jeff Foley. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package sources
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/OWASP/Amass/amass/core"
+	"github.com/OWASP/Amass/amass/utils"
+)
+
+// ViewDNS is the Service that handles access to the ViewDNS data source.
+type ViewDNS struct {
+	core.BaseService
+
+	SourceType string
+	RateLimit  time.Duration
+}
+
+// NewViewDNS returns he object initialized, but not yet started.
+func NewViewDNS(config *core.Config, bus *core.EventBus) *ViewDNS {
+	v := &ViewDNS{
+		SourceType: core.SCRAPE,
+		RateLimit:  10 * time.Second,
+	}
+
+	v.BaseService = *core.NewBaseService(v, "ViewDNS", config, bus)
+	return v
+}
+
+// OnStart implements the Service interface
+func (v *ViewDNS) OnStart() error {
+	v.BaseService.OnStart()
+
+	go v.processRequests()
+	return nil
+}
+
+func (v *ViewDNS) processRequests() {
+	last := time.Now().Truncate(10 * time.Minute)
+
+	for {
+		select {
+		case <-v.Quit():
+			return
+		case dns := <-v.DNSRequestChan():
+			if v.Config().IsDomainInScope(dns.Domain) {
+				if time.Now().Sub(last) < v.RateLimit {
+					time.Sleep(v.RateLimit)
+				}
+				last = time.Now()
+				v.executeDNSQuery(dns.Domain)
+				last = time.Now()
+			}
+		case whois := <-v.WhoisRequestChan():
+			if v.Config().IsDomainInScope(whois.Domain) {
+				if time.Now().Sub(last) < v.RateLimit {
+					time.Sleep(v.RateLimit)
+				}
+				last = time.Now()
+				v.executeWhoisQuery(whois.Domain)
+				last = time.Now()
+			}
+		case <-v.AddrRequestChan():
+		case <-v.ASNRequestChan():
+		}
+	}
+}
+
+func (v *ViewDNS) executeDNSQuery(domain string) {
+	var unique []string
+
+	u := "http://viewdns.info/iphistory/?domain=" + domain
+	// The ViewDNS IP History lookup sometimes reveals interesting results
+	page, err := utils.RequestWebPage(u, nil, nil, "", "")
+	if err != nil {
+		v.Config().Log.Printf("%s: %s: %v", v.String(), u, err)
+		return
+	}
+
+	// Look for IP addresses in the web page returned
+	re := regexp.MustCompile(utils.IPv4RE)
+	for _, sd := range re.FindAllString(page, -1) {
+		addr := utils.NewUniqueElements(unique, sd)
+
+		if len(addr) > 0 {
+			v.Bus().Publish(core.NewAddrTopic, &core.AddrRequest{
+				Address: addr[0],
+				Domain:  domain,
+				Tag:     v.SourceType,
+				Source:  v.String(),
+			})
+		}
+	}
+}
+
+func (v *ViewDNS) executeWhoisQuery(domain string) {
+	u := v.getURL(domain)
+	page, err := utils.RequestWebPage(u, nil, nil, "", "")
+	if err != nil {
+		v.Config().Log.Printf("%s: %s: %v", v.String(), u, err)
+		return
+	}
+	// Pull the table we need from the page content
+	table := getViewDNSTable(page)
+	if table == "" {
+		v.Config().Log.Printf("%s: %s: Failed to discover the table of results", v.String(), u)
+		return
+	}
+	// Get the list of domain names discovered through the reverse DNS service
+	re := regexp.MustCompile("<tr><td>([a-zA-Z0-9]{1}[a-zA-Z0-9-]{0,61}[a-zA-Z0-9]{1}[.]{1}[a-zA-Z0-9-]+)</td><td>")
+	subs := re.FindAllStringSubmatch(table, -1)
+	for _, match := range subs {
+		sub := match[1]
+		if sub == "" {
+			continue
+		}
+
+		v.Bus().Publish(core.NewWhoisTopic, &core.WhoisRequest{
+			Domain: strings.TrimSpace(sub),
+			Tag:    v.SourceType,
+			Source: v.String(),
+		})
+	}
+}
+
+func (v *ViewDNS) getURL(domain string) string {
+	format := "http://viewdns.info/reversewhois/?q=%s"
+
+	return fmt.Sprintf(format, domain)
+}
+
+func getViewDNSTable(page string) string {
+	var begin, end int
+
+	s := page
+	for i := 0; i < 4; i++ {
+		b := strings.Index(s, "<table")
+		if b == -1 {
+			return ""
+		}
+		begin += b + 6
+
+		if e := strings.Index(s[b:], "</table>"); e != -1 {
+			end = begin + e
+		} else {
+			return ""
+		}
+		s = page[end+8:]
+	}
+	i := strings.Index(page[begin:end], "<table")
+	i = strings.Index(page[begin+i+6:end], "<table")
+	return page[begin+i : end]
+}
