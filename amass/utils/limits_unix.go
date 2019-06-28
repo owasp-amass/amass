1be9ecd41800ad701f0116039c048c30cbc1bb68
caffix <caffix@gmail.com>
2019-06-28 13:27:56 -0400

    fixed a type mismatch bug

--- amass/utils/limits_unix.go
+++ amass/utils/limits_unix.go
@@ -12,14 +12,15 @@ import (
 // GetFileLimit attempts to raise the ulimit to the maximum hard limit and returns that value.
 func GetFileLimit() int {
 	limit := 10000
-	var lim syscall.Rlimit
+	infinity := uint64(syscall.RLIM_INFINITY)
 
+	var lim syscall.Rlimit
 	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &lim); err == nil {
 		lim.Cur = lim.Max
 		syscall.Setrlimit(syscall.RLIMIT_NOFILE, &lim)
 	}
 
-	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &lim); err == nil && lim.Cur != syscall.RLIM_INFINITY {
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &lim); err == nil && lim.Cur != infinity {
 		limit = int(lim.Cur)
 	}
 	return limit
