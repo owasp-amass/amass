ded548216c587499295e7e13fbb4f7de5112ad08
caffix <caffix@gmail.com>
2019-06-28 21:03:25 -0400

    version 3.0.16 release

--- amass/amass.go
+++ amass/amass.go
@@ -33,7 +33,7 @@ const Banner = `
 
 const (
 	// Version is used to display the current version of Amass.
-	Version = "3.0.15"
+	Version = "3.0.16"
 
 	// Author is used to display the Amass Project Team.
 	Author = "OWASP Amass Project - @owaspamass"
