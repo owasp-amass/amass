184619b54b8abdb474ad1b3366b0e7d583607585
fork-while-fork <git@norse.pw>
2019-07-18 17:50:52 -0400

    Add tests for new fuctions

--- amass/utils/misc_test.go
+++ amass/utils/misc_test.go
@@ -93,7 +93,50 @@ func TestRemoveAsteriskLabel(t *testing.T) {
 		if s != tt.expected && s != "" {
 			t.Errorf("Error Event %s: was expecting \"\" or %s, got %s", tt.name, tt.expected, tt.event)
 		}
-
 	}
+}
 
+func TestExpandMask(t *testing.T) {
+	tests := []struct {
+		name     string
+		event    string
+		expected int
+	}{
+
+		{"Test 1: All", "test?a", 37},
+		{"Test 2: Letter (?l)", "test?l", 26},
+		{"Test 3: Letter (?u)", "test?u", 26},
+		{"Test 4: Digit", "test?d", 10},
+		{"Test 5: Special", "test?s", 1},
+		{"Test 6: Multiple All", "test?a?a", 1369},
+		{"Test 7: Multiple Letters (?l)", "test?l?l", 676},
+		{"Test 8: Multiple Letters (?u)", "test?u?u", 676},
+		{"Test 9: Multiple Digits", "test?d?d", 100},
+		{"Test 10: Multiple Special", "test?s?s", 1},
+		{"Test 11: Mixed Mask", "test?a?l?d", 9620},
+		{"Test 12: Mask too long", "test?a?a?a?a?a", 0},
+	}
+	for _, tt := range tests {
+		s, _ := ExpandMask(tt.event)
+		if len(s) != tt.expected {
+			t.Errorf("Error Event %s: was expecting %d, got %d", tt.name, tt.expected, len(s))
+		}
+	}
+}
+
+func TestExpandMaskWordlist(t *testing.T) {
+	tests := []struct {
+		name     string
+		event    []string
+		expected int
+	}{
+
+		{"Test 1: Wordlist", []string{"?a", "?d", "?u", "?l", "?s"}, 100},
+	}
+	for _, tt := range tests {
+		s, _ := ExpandMaskWordlist(tt.event)
+		if len(s) != tt.expected {
+			t.Errorf("Error Event %s: was expecting %d, got %d", tt.name, tt.expected, len(s))
+		}
+	}
 }
