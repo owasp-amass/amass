9bb7ec615e058eca89946e1d8816f3d030ffe7a8
vltraheaven <vltraheaven@protonmail.com>
2019-07-30 23:30:41 -0700

    Created alienvault_test.go unit test file for AlienVault source

---
+++ services/sources/alienvault_test.go
@@ -0,0 +1,38 @@
+package sources
+
+import (
+	"testing"
+
+	"github.com/OWASP/Amass/config"
+	"github.com/OWASP/Amass/requests"
+	"github.com/OWASP/Amass/resolvers"
+)
+
+func TestAlienVault(t *testing.T) {
+	if *networkTest == false || *configPath == "" {
+		return
+	}
+
+	cfg := setupConfig(domainTest)
+
+	API := new(config.APIKey)
+	API = cfg.GetAPIKey("alienvault")
+
+	if API == nil || API.Key == "" {
+		t.Errorf("API key data was not provided")
+		return
+	}
+
+	bus, out := setupEventBus(requests.NewNameTopic)
+	defer bus.Stop()
+
+	pool := resolvers.NewResolverPool(nil)
+	defer pool.Stop()
+
+	srv := NewAlienVault(cfg, bus, pool)
+
+	result := testService(srv, out)
+	if result < expectedTest {
+		t.Errorf("Found %d names, expected at least %d instead", result, expectedTest)
+	}
+}
