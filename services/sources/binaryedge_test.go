97b636c3b756a9cb9e3b1396db75113b6216574a
vltraheaven <vltraheaven@protonmail.com>
2019-07-30 23:35:54 -0700

    Created binaryedge_test.go unit test file for BinaryEdge source

---
+++ services/sources/binaryedge_test.go
@@ -0,0 +1,38 @@
+package sources
+
+import (
+	"testing"
+
+	"github.com/OWASP/Amass/config"
+	"github.com/OWASP/Amass/requests"
+	"github.com/OWASP/Amass/resolvers"
+)
+
+func TestBinaryEdge(t *testing.T) {
+	if *networkTest == false || *configPath == "" {
+		return
+	}
+
+	cfg := setupConfig(domainTest)
+
+	API := new(config.APIKey)
+	API = cfg.GetAPIKey("binaryedge")
+
+	if API == nil || API.Key == "" {
+		t.Errorf("API key data was not provided")
+		return
+	}
+
+	bus, out := setupEventBus(requests.NewNameTopic)
+	defer bus.Stop()
+
+	pool := resolvers.NewResolverPool(nil)
+	defer pool.Stop()
+
+	srv := NewBinaryEdge(cfg, bus, pool)
+
+	result := testService(srv, out)
+	if result < expectedTest {
+		t.Errorf("Found %d names, expected at least %d instead", result, expectedTest)
+	}
+}
