9d81c59a67f8d1287212c3b42ce68a4d13f310c6
caffix <caffix@gmail.com>
2019-09-14 23:00:10 -0400

    added support to perform AS and IP addresses information queries

--- services/sources/umbrella.go
+++ services/sources/umbrella.go
@@ -6,6 +6,7 @@ package sources
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"strings"
 	"time"
 
@@ -15,6 +16,7 @@ import (
 	"github.com/OWASP/Amass/requests"
 	"github.com/OWASP/Amass/resolvers"
 	"github.com/OWASP/Amass/services"
+	sf "github.com/OWASP/Amass/stringfilter"
 	"github.com/OWASP/Amass/stringset"
 )
 
@@ -25,6 +27,7 @@ type Umbrella struct {
 	API        *config.APIKey
 	SourceType string
 	RateLimit  time.Duration
+	filter     *sf.StringFilter
 }
 
 // NewUmbrella returns he object initialized, but not yet started.
@@ -32,6 +35,7 @@ func NewUmbrella(cfg *config.Config, bus *eb.EventBus, pool *resolvers.ResolverP
 	u := &Umbrella{
 		SourceType: requests.API,
 		RateLimit:  500 * time.Millisecond,
+		filter:     sf.NewStringFilter(),
 	}
 
 	u.BaseService = *services.NewBaseService(u, "Umbrella", cfg, bus, pool)
@@ -49,6 +53,8 @@ func (u *Umbrella) OnStart() error {
 		)
 	}
 
+	u.Bus().Subscribe(requests.NewAddrTopic, u.SendAddrRequest)
+	u.Bus().Subscribe(requests.IPToASNTopic, u.SendASNRequest)
 	go u.processRequests()
 	return nil
 }
@@ -56,6 +62,7 @@ func (u *Umbrella) OnStart() error {
 func (u *Umbrella) processRequests() {
 	last := time.Now()
 
+loop:
 	for {
 		select {
 		case <-u.Quit():
@@ -69,8 +76,27 @@ func (u *Umbrella) processRequests() {
 				u.executeDNSQuery(req.Domain)
 				last = time.Now()
 			}
-		case <-u.AddrRequestChan():
-		case <-u.ASNRequestChan():
+		case req := <-u.AddrRequestChan():
+			if time.Now().Sub(last) < u.RateLimit {
+				time.Sleep(u.RateLimit)
+			}
+			last = time.Now()
+			u.executeAddrQuery(req.Address)
+			last = time.Now()
+		case req := <-u.ASNRequestChan():
+			if req.Address == "" && req.ASN == 0 {
+				continue loop
+			}
+			if time.Now().Sub(last) < u.RateLimit {
+				time.Sleep(u.RateLimit)
+			}
+			last = time.Now()
+			if req.Address != "" {
+				u.executeASNAddrQuery(req)
+			} else {
+				u.executeASNQuery(req)
+			}
+			last = time.Now()
 		case req := <-u.WhoisRequestChan():
 			if u.Config().IsDomainInScope(req.Domain) {
 				if time.Now().Sub(last) < u.RateLimit {
@@ -85,65 +111,194 @@ func (u *Umbrella) processRequests() {
 }
 
 func (u *Umbrella) executeDNSQuery(domain string) {
-	re := u.Config().DomainRegex(domain)
-	if re == nil || u.API == nil || u.API.Key == "" {
+	if u.API == nil || u.API.Key == "" {
 		return
 	}
 
 	u.SetActive()
 	headers := u.restHeaders()
-	url := u.patternSearchRestURL(domain)
+	url := u.restDNSURL(domain)
 	page, err := http.RequestWebPage(url, nil, headers, "", "")
 	if err != nil {
 		u.Bus().Publish(requests.LogTopic, fmt.Sprintf("%s: %s: %v", u.String(), url, err))
 		return
 	}
-
-	for _, name := range re.FindAllString(page, -1) {
-		u.Bus().Publish(requests.NewNameTopic, &requests.DNSRequest{
-			Name:   cleanName(name),
-			Domain: domain,
-			Tag:    u.SourceType,
-			Source: u.String(),
-		})
+	// Extract the subdomain names from the REST API results
+	var subs struct {
+		Matches []struct {
+			Name string `json:"name"`
+		} `json:"matches"`
 	}
-
-	url = u.occurrencesRestURL(domain)
-	page, err = http.RequestWebPage(url, nil, headers, "", "")
-	if err != nil {
-		u.Bus().Publish(requests.LogTopic, fmt.Sprintf("%s: %s: %v", u.String(), url, err))
+	if err := json.Unmarshal([]byte(page), &subs); err != nil {
 		return
 	}
 
-	for _, d := range u.Config().Domains() {
-		re := u.Config().DomainRegex(d)
-		for _, sd := range re.FindAllString(page, -1) {
+	for _, m := range subs.Matches {
+		if d := u.Config().WhichDomain(m.Name); d != "" {
 			u.Bus().Publish(requests.NewNameTopic, &requests.DNSRequest{
-				Name:   cleanName(sd),
+				Name:   m.Name,
 				Domain: d,
 				Tag:    u.SourceType,
 				Source: u.String(),
 			})
 		}
 	}
+}
+
+func (u *Umbrella) executeAddrQuery(addr string) {
+	if u.API == nil || u.API.Key == "" {
+		return
+	}
+	if addr == "" || u.filter.Duplicate(addr) {
+		return
+	}
 
 	u.SetActive()
-	url = u.relatedRestURL(domain)
-	page, err = http.RequestWebPage(url, nil, headers, "", "")
+	headers := u.restHeaders()
+	url := u.restAddrURL(addr)
+	page, err := http.RequestWebPage(url, nil, headers, "", "")
 	if err != nil {
 		u.Bus().Publish(requests.LogTopic, fmt.Sprintf("%s: %s: %v", u.String(), url, err))
 		return
 	}
+	// Extract the subdomain names from the REST API results
+	var ip struct {
+		Records []struct {
+			Data string `json:"rr"`
+		} `json:"records"`
+	}
+	if err := json.Unmarshal([]byte(page), &ip); err != nil {
+		return
+	}
 
-	for _, d := range u.Config().Domains() {
-		re := u.Config().DomainRegex(d)
-		for _, sd := range re.FindAllString(page, -1) {
-			u.Bus().Publish(requests.NewNameTopic, &requests.DNSRequest{
-				Name:   cleanName(sd),
-				Domain: d,
-				Tag:    u.SourceType,
-				Source: u.String(),
-			})
+	for _, record := range ip.Records {
+		if name := resolvers.RemoveLastDot(record.Data); name != "" {
+			if domain := u.Config().WhichDomain(name); domain != "" {
+				u.Bus().Publish(requests.NewNameTopic, &requests.DNSRequest{
+					Name:   name,
+					Domain: domain,
+					Tag:    u.SourceType,
+					Source: u.String(),
+				})
+			}
+		}
+	}
+}
+
+func (u *Umbrella) executeASNAddrQuery(req *requests.ASNRequest) {
+	if u.API == nil || u.API.Key == "" {
+		return
+	}
+
+	u.SetActive()
+	headers := u.restHeaders()
+	url := u.restAddrToASNURL(req.Address)
+	page, err := http.RequestWebPage(url, nil, headers, "", "")
+	if err != nil {
+		u.Bus().Publish(requests.LogTopic, fmt.Sprintf("%s: %s: %v", u.String(), url, err))
+		return
+	}
+	// Extract the AS information from the REST API results
+	var as []struct {
+		Date        string `json:"creation_date"`
+		Registry    int    `json:"ir"`
+		Description string `json:"description"`
+		ASN         int    `json:"asn"`
+		CIDR        string `json:"cidr"`
+	}
+	if err := json.Unmarshal([]byte(page), &as); err != nil || len(as) == 0 {
+		return
+	}
+
+	created, err := time.Parse("2006-01-02", as[0].Date)
+	if err != nil {
+		return
+	}
+
+	var registry string
+	switch as[0].Registry {
+	case 1:
+		registry = "AfriNIC"
+	case 2:
+		registry = "APNIC"
+	case 3:
+		registry = "ARIN"
+	case 4:
+		registry = "LACNIC"
+	case 5:
+		registry = "RIPE NCC"
+	default:
+		registry = "N/A"
+	}
+
+	req.ASN = as[0].ASN
+	req.Prefix = as[0].CIDR
+	req.Registry = registry
+	req.AllocationDate = created
+	req.Description = as[0].Description
+	req.Tag = u.SourceType
+	req.Source = u.String()
+	if req.Netblocks == nil {
+		req.Netblocks = stringset.New()
+		req.Netblocks.Insert(strings.TrimSpace(req.Prefix))
+		time.Sleep(u.RateLimit)
+		u.executeASNQuery(req)
+	}
+	u.Bus().Publish(requests.NewASNTopic, req)
+}
+
+func (u *Umbrella) executeASNQuery(req *requests.ASNRequest) {
+	if u.API == nil || u.API.Key == "" {
+		return
+	}
+
+	u.SetActive()
+	headers := u.restHeaders()
+	url := u.restASNToCIDRsURL(req.ASN)
+	page, err := http.RequestWebPage(url, nil, headers, "", "")
+	if err != nil {
+		u.Bus().Publish(requests.LogTopic, fmt.Sprintf("%s: %s: %v", u.String(), url, err))
+		return
+	}
+	// Extract the netblock information from the REST API results
+	var netblock []struct {
+		CIDR string `json:"cidr"`
+		Geo  struct {
+			CountryName string `json:"country_name"`
+			CountryCode string `json:"country_code"`
+		} `json:"geo"`
+	}
+	if err := json.Unmarshal([]byte(page), &netblock); err != nil || len(netblock) == 0 {
+		return
+	}
+
+	if req.Netblocks == nil {
+		req.Netblocks = stringset.New()
+	}
+
+	for _, nb := range netblock {
+		req.Netblocks.Insert(strings.TrimSpace(nb.CIDR))
+		if nb.CIDR == req.Prefix {
+			req.CC = nb.Geo.CountryCode
+		}
+	}
+	// If no basic AS info exists, then obtain an IP and query
+	if req.Prefix == "" {
+		addr, _, err := net.ParseCIDR(netblock[0].CIDR)
+
+		if err == nil {
+			req.Address = addr.String()
+			req.CC = netblock[0].Geo.CountryCode
+			time.Sleep(u.RateLimit)
+			u.executeASNAddrQuery(req)
+			return
+		}
+	}
+	// Finish populating the AS info in the request
+	for _, nb := range netblock {
+		if nb.CIDR == req.Prefix {
+			req.CC = nb.Geo.CountryCode
+			break
 		}
 	}
 }
@@ -336,14 +491,18 @@ func (u *Umbrella) reverseWhoisByEmailURL(emails ...string) string {
 	return u.whoisBaseURL() + `emails?emailList=` + emailQuery
 }
 
-func (u *Umbrella) patternSearchRestURL(domain string) string {
+func (u *Umbrella) restDNSURL(domain string) string {
 	return `https://investigate.api.umbrella.com/search/.*[.]` + domain + "?start=-30days&limit=1000"
 }
 
-func (u *Umbrella) occurrencesRestURL(domain string) string {
-	return "https://investigate.api.umbrella.com/recommendations/name/" + domain + ".json"
+func (u *Umbrella) restAddrURL(addr string) string {
+	return "https://investigate.api.umbrella.com/pdns/ip/" + addr + "?recordType=A,AAAA"
 }
 
-func (u *Umbrella) relatedRestURL(domain string) string {
-	return "https://investigate.api.umbrella.com/links/name/" + domain + ".json"
+func (u *Umbrella) restAddrToASNURL(addr string) string {
+	return fmt.Sprintf("https://investigate.api.umbrella.com/bgp_routes/ip/%s/as_for_ip.json", addr)
+}
+
+func (u *Umbrella) restASNToCIDRsURL(asn int) string {
+	return fmt.Sprintf("https://investigate.api.umbrella.com/bgp_routes/asn/%d/prefixes_for_asn.json", asn)
 }
