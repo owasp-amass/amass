1424222bbb71945143319f73a55c2bbe3481ab1c
Kian <kianjamali123@gmail.com>
2019-10-04 18:42:20 -0700

    Update: Added the basic implementation of Pastebin service

--- services/sources/pastebin.go
+++ services/sources/pastebin.go
@@ -2,8 +2,12 @@ package sources
 
 import (
 	"time"
-	
+
 	"github.com/OWASP/Amass/services"
+	"github.com/OWASP/Amass/config"
+	eb "github.com/OWASP/Amass/eventbus"
+	"github.com/OWASP/Amass/resolvers"
+	"github.com/OWASP/Amass/requests"
 )
 
 // Pastebin is the Service that handles access to the CertSpotter data source.
@@ -12,4 +16,52 @@ type Pastebin struct {
 
 	SourceType string
 	RateLimit  time.Duration
+}
+
+// NewPastebin returns he object initialized, but not yet started.
+func NewPastebin(cfg *config.Config, bus *eb.EventBus, pool *resolvers.ResolverPool) *Pastebin {
+	p := &Pastebin{
+		SourceType: requests.API,
+		RateLimit:  3 * time.Second,
+	}
+
+	p.BaseService = *services.NewBaseService(t, "Pastebin", cfg, bus, pool)
+	return p
+}
+
+
+// OnStart implements the Service interface
+func (p *Pastebin) OnStart() error {
+	p.BaseService.OnStart()
+
+
+	go p.processRequests()
+	return nil
+}
+
+func (p *Pastebin) processRequests() {
+	last := time.Now()
+
+	for {
+		select {
+		case <-p.Quit():
+			return
+		case req := <-p.DNSRequestChan():
+			if p.Config().IsDomainInScope(req.Domain) {
+				if time.Now().Sub(last) < p.RateLimit {
+					time.Sleep(p.RateLimit)
+				}
+				last = time.Now()
+				p.executeQuery(req.Domain)
+				last = time.Now()
+			}
+		case <-p.AddrRequestChan():
+		case <-p.ASNRequestChan():
+		case <-p.WhoisRequestChan():
+		}
+	}
+}
+
+func (p *Pastebin) executeQuery(domain string) {
+
 }
