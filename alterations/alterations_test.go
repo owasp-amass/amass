c02f6592fe394a4131dac3ceb6a90e29977ddf1d
caffix <caffix@github.com>
2019-12-03 11:50:44 -0500

    the tests do not currently work

--- alterations/alterations_test.go
+++ alterations/alterations_test.go
@@ -1,79 +1,87 @@
-package main
-
+package alterations
+/*
 import (
-	"github.com/OWASP/Amass/v3/alterations"
 	"testing"
 )
 
-var subdomain = "fa2ke.owasp.org"
-
 func checkAlterations(generated []string, expected []string) bool {
-	return len(generated) == len(expected);
+	return len(generated) == len(expected)
 }
 
-func createState() *alterations.State {
-	Wordlist := []string{"test", "abc", "123"}
-	altState := alterations.NewState(Wordlist)
+func createState() *State {
+	altState := NewState([]string{"test", "abc", "123"})
+
 	altState.MinForWordFlip = 3
 	altState.EditDistance = 2
+
 	return altState
 }
 
 func TestAlterations(t *testing.T) {
-	domain := "www.owasp.org"
-	altState := createState(wordlist)
+	fake := "fa2ke.owasp.org"
+	altState := createState()
+
 	t.Run("Fuzzy Label subtest", func(t *testing.T) {
-		expectedSubdomains := []string{
+		expected := []string{
 			"uzyxw.owasp.org",
 			"m-1e.owasp.org",
 			"0ihgf.owasp.org",
 		}
-		fuzzyLabel := altState.FuzzyLabelSearches(subdomain)
-		if !checkAlterations(fuzzyLabel, expectedSubdomains) {
+
+		fuzzyLabel := altState.FuzzyLabelSearches(fake)
+		if !checkAlterations(fuzzyLabel, expected) {
 			t.Errorf("Could not generate all fuzzy label alterations")
 		}
 	})
+
 	t.Run("flip number label subtest", func(t *testing.T) {
-		expectedSubdomains := []string{
-			"fa7ke.owasp.org",
-			"fa8ke.owasp.org",
-			"fa3ke.owasp.org",
+		expected := []string{
+			"fa0ke.owasp.org",
 		}
-		flipNumbers := altState.FlipNumbers(subdomain)
-		if !checkAlterations(flipNumbers, expectedSubdomains) {
+
+		flipNumbers := altState.FlipNumbers(fake)
+		if !checkAlterations(flipNumbers, expected) {
 			t.Errorf("Could not generate all Flip Number")
 		}
 	})
+
 	t.Run("append number label subtest", func(t *testing.T) {
-		expectedSubdomains := []string{
-			"fa2ke2.owasp.org",
-			"fa2ke3.owasp.org",
-			"fa2ke4.owasp.org",
+		var expected []string
+		for i := 0; i < 10; i++ {
+			expected = append(expected, "fa2ke" + strconv.Itoa(i) + ".owasp.org")
 		}
-		appendNumbers := altState.AppendNumbers(subdomain)
-		if !checkAlterations(appendNumbers, expectedSubdomains) {
+
+		appendNumbers := altState.AppendNumbers(fake)
+		if !checkAlterations(appendNumbers, expected) {
 			t.Errorf("Could not generate all append number label alterations")
 		}
 	})
+
 	t.Run("flip word label subtest", func(t *testing.T) {
-		expectedSubdomains := []string{}
-		flipWords := altState.FlipWords(subdomain)
-		if !checkAlterations(flipWords, expectedSubdomains) {
+		expected := []string{}
+		flipWords := altState.FlipWords(fake)
+
+		if !checkAlterations(flipWords, expected) {
 			t.Errorf("Could not generate all flip words label alterations")
 		}
 	})
+
 	t.Run("suffix label subtest", func(t *testing.T) {
-		expectedSubdomains := []string{}
-		addSuffix := altState.AddSuffixWord(subdomain)
-		if !checkAlterations(addSuffix, expectedSubdomains) {
+		expected := []string{}
+		addSuffix := altState.AddSuffixWord(fake)
+
+		if !checkAlterations(addSuffix, expected) {
 			t.Errorf("Could not generate all add suffix label alterations")
 		}
 	})
+
 	t.Run("prefix number label subtest", func(t *testing.T) {
-		expectedSubdomains := []string{}
-		addPrefix := altState.AddPrefixWord(subdomain)
-		if !checkAlterations(addPrefix, expectedSubdomains) {
+		expected := []string{}
+		addPrefix := altState.AddPrefixWord(fake)
+
+		if !checkAlterations(addPrefix, expected) {
 			t.Errorf("Could not generate all add prefix number label alterations")
 		}
 	})
 }
+*/
