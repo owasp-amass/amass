55231c9033eaf13932b5737bc8ed8512b94869a4
fork-while-fork <git@norse.pw>
2019-12-04 11:37:04 -0500

    Fix issue where amass hangs after printing the summary
    Amass was hanging on wg.Wait() because submitKnownNames was
    not exiting. This commit implements a workaround that avoids
    the use of GetOutput(). Also fixed some graph tests that were
    broken

--- graph/viz_test.go
+++ graph/viz_test.go
@@ -11,13 +11,12 @@ func VizTest(t *testing.T) {
 
 	for _, tt := range graphTest {
 		t.Run("Testing VizData...", func(t *testing.T) {
-			g.InsertNodeIfNotExist(tt.ID, "test node")
 			err := g.InsertA(tt.FQDN, tt.Addr, tt.Source, tt.Tag, tt.EventID)
 
 			if err != nil {
 				t.Errorf("Error inserting A record.\n%v", err)
 			}
-			gotNode, gotEdge := g.VizData(tt.UUID)
+			gotNode, gotEdge := g.VizData(tt.EventID)
 			if gotNode == nil {
 				t.Errorf("Failed to obtain node.\n%v", gotNode)
 			}
