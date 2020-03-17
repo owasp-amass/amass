84035227d6a7cf0d76649ee116586141df849ca7
caffix <caffix@github.com>
2020-03-16 22:53:06 -0400

    v3.5.0 release

--- format/print.go
+++ format/print.go
@@ -31,7 +31,7 @@ const Banner = `
 
 const (
 	// Version is used to display the current version of Amass.
-	Version = "v3.4.4"
+	Version = "v3.5.0"
 
 	// Author is used to display the Amass Project Team.
 	Author = "OWASP Amass Project - @owaspamass"
