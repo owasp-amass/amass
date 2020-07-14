ff338b4d99be2616c55c70f934e070997fffb2f7
caffix <caffix@users.noreply.github.com>
2020-07-13 23:37:07 -0400

    ported more data source implementations to ADS scripts

---
+++ resources/scripts/api/virustotal.ads
@@ -0,0 +1,81 @@
+-- Copyright 2017 Jeff Foley. All rights reserved.
+-- Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+local json = require("json")
+
+name = "VirusTotal"
+type = "api"
+
+function start()
+    setratelimit(15)
+end
+
+function vertical(ctx, domain)
+    local haskey = true
+    if (api == nil or api.key == nil or api.key == "") then
+        haskey = false
+    end
+
+    local resp
+    local vurl = buildurl(domain)
+    if haskey then
+        vurl = apiurl(domain)
+    end
+    -- Check if the response data is in the graph database
+    if (api.ttl ~= nil and api.ttl > 0) then
+        resp = obtain_response(vurl, api.ttl)
+    end
+
+    if (resp == nil or resp == "") then
+        local err
+
+        resp, err = request({
+            url=vurl,
+            headers={['Content-Type']="application/json"},
+        })
+        if (err ~= nil and err ~= "") then
+            return
+        end
+
+        if (api.ttl ~= nil and api.ttl > 0) then
+            cache_response(vurl, resp)
+        end
+    end
+
+    local d = json.decode(resp)
+    if haskey then
+        if d['response_code'] ~= 1 then
+            log(ctx, name .. ": " .. vurl .. ": Response code " .. d['response_code'] .. ": " .. d['verbose_msg'])
+            return
+        end
+
+        for i, sub in pairs(d.subdomains) do
+            sendnames(ctx, sub)
+        end
+    else
+        for i, data in pairs(d.data) do
+            if data.type == "domain" then
+                sendnames(ctx, data.id)
+            end
+        end
+    end
+end
+
+function buildurl(domain)
+    return "https://www.virustotal.com/ui/domains/" .. domain .. "/subdomains?limit=40"
+end
+
+function apiurl(domain)
+    return "https://www.virustotal.com/vtapi/v2/domain/report?apikey=" .. api.key .. "&domain=" .. domain
+end
+
+function sendnames(ctx, content)
+    local names = find(content, subdomainre)
+    if names == nil then
+        return
+    end
+
+    for i, v in pairs(names) do
+        newname(ctx, v)
+    end
+end
