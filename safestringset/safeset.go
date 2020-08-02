c7e6071de3b8c86a7adb9514b7011c6d5036554b
Adam Greene <copyright@mzpqnxow.com>
2020-08-02 05:45:11 -0400

    Update for CodeClimate warnings

--- safestringset/safeset.go
+++ safestringset/safeset.go
@@ -6,18 +6,16 @@ import (
 	"sync"
 )
 
-// Set that is concurrency safe, based on a mixture of stringset.Set
-// and https://www.davidkaya.com/sets-in-golang/ with added locking
-// Use only when you need to share a stringset.Set-like instance
-// across goroutines. Implemented as an option for fixing Amass
-// issue #445
-
 type SafeSet struct {
+	// SafeSet implements set operations for string values safe for concurrent use
+	// Implemented as an option for fixing Amass issue #445
 	m map[string]bool
 	sync.RWMutex
 }
 
+// New returns a SafeSet containing the values provided in the arguments.
 func New(initial ...string) *SafeSet {
+
 	s := &SafeSet{
 		m: make(map[string]bool),
 	}
@@ -29,19 +27,19 @@ func New(initial ...string) *SafeSet {
 	return s
 }
 
-// Deduplicate utilizes the Set type to generate a unique list of strings from the input slice.
+// Deduplicate utilizes the SafeSet type to generate a unique list of strings from the input slice.
 func Deduplicate(input []string) []string {
 	return New(input...).Slice()
 }
 
-// Add add
+// Insert adds the element string argument to the receiver SafeSet.
 func (s *SafeSet) Insert(item string) {
 	s.Lock()
 	defer s.Unlock()
 	s.m[item] = true
 }
 
-// InsertMany adds all the elements strings into the receiver Set.
+// InsertMany adds all the elements strings into the receiver SafeSet.
 func (s *SafeSet) InsertMany(elements ...string) {
 	for _, i := range elements {
 		s.Insert(i)
@@ -55,7 +53,7 @@ func (s *SafeSet) Remove(item string) {
 	delete(s.m, item)
 }
 
-// Union adds all the elements from the other Set argument into the receiver Set.
+// Union adds all the elements from the other SafeSet argument into the receiver SafeSet.
 func (s *SafeSet) Union(other *SafeSet) {
 	for k := range other.m {
 		s.Insert(k)
@@ -70,7 +68,7 @@ func (s *SafeSet) Has(item string) bool {
 	return ok
 }
 
-// Subtract removes all elements in the other Set argument from the receiver Set.
+// Subtract removes all elements in the other SafeSet argument from the receiver SafeSet.
 func (s *SafeSet) Subtract(other *SafeSet) {
 	for item := range other.m {
 		if s.Has(item) {
@@ -79,8 +77,8 @@ func (s *SafeSet) Subtract(other *SafeSet) {
 	}
 }
 
-// Intersect causes the receiver Set to only contain elements also found in the
-// other Set argument.
+// Intersect causes the receiver SafeSet to only contain elements also found in the
+// other SafeSet argument.
 func (s *SafeSet) Intersect(other *SafeSet) {
 	intersect := New()
 
@@ -106,12 +104,12 @@ func (s *SafeSet) String() string {
 	return strings.Join(s.Slice(), ",")
 }
 
-// Len returns the number of items in a set.
+// Len returns the number of items in a SafeSet.
 func (s *SafeSet) Len() int {
 	return len(s.List())
 }
 
-// Slice returns a string slice that contains all the elements in the Set.
+// Slice returns a string slice that contains all the elements in the SafeSet.
 func (s *SafeSet) Slice() []string {
 	var i uint64
 	s.RLock()
@@ -127,7 +125,7 @@ func (s *SafeSet) Slice() []string {
 	return k
 }
 
-// Set implements the flag.Value interface.
+// SafeSet implements the flag.Value interface.
 func (s *SafeSet) SafeSet(input string) error {
 	if input == "" {
 		return fmt.Errorf("String parsing failed")
@@ -140,7 +138,7 @@ func (s *SafeSet) SafeSet(input string) error {
 	return nil
 }
 
-// Set returns a slice of all items
+// List returns a slice of all items.
 func (s *SafeSet) List() []string {
 	s.RLock()
 	defer s.RUnlock()
