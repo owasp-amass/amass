3b148225f27eec105e18f4f4ff346a894e621515
Miguel Ángel Jimeno <miguelangel4b@gmail.com>
2020-08-24 20:00:34 +0200

    graph/viz: fix viz test
    Signed-off-by: Miguel Ángel Jimeno <miguelangel4b@gmail.com>

--- graph/viz_test.go
+++ graph/viz_test.go
@@ -4,17 +4,27 @@ import (
 	"testing"
 )
 
-func VizTest(t *testing.T) {
+func TestViz(t *testing.T) {
 	g := NewGraph(NewCayleyGraphMemory())
 
-	for _, tt := range graphTest {
+	tt := []struct {
+		fqdn    string
+		addr    string
+		source  string
+		tag     string
+		eventID string
+	}{
+		{fqdn: "dev.example.domain", addr: "127.0.0.1", source: "test", tag: "foo", eventID: "barbazz"},
+	}
+
+	for _, tc := range tt {
 		t.Run("Testing VizData...", func(t *testing.T) {
-			err := g.InsertA(tt.FQDN, tt.Addr, tt.Source, tt.Tag, tt.EventID)
+			err := g.InsertA(tc.fqdn, tc.addr, tc.source, tc.tag, tc.eventID)
 
 			if err != nil {
 				t.Errorf("Error inserting A record.\n%v", err)
 			}
-			gotNode, gotEdge := g.VizData(tt.EventID)
+			gotNode, gotEdge := g.VizData(tc.eventID)
 			if gotNode == nil {
 				t.Errorf("Failed to obtain node.\n%v", gotNode)
 			}
