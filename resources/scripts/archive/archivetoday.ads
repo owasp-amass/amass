ea46f501c6432fdcecf5ecd40d374f00ff8cf264
shelld3v <revert@revertarjun.com>
2021-02-03 18:03:15 +0700

    Added IPv4Info and ArchiveToday as data sources

---
+++ resources/scripts/archive/archivetoday.ads
@@ -0,0 +1,17 @@
+-- Copyright 2017 Jeff Foley. All rights reserved.
+-- Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+name = "ArchiveToday"
+type = "archive"
+
+function start()
+    setratelimit(10)
+end
+
+function vertical(ctx, domain)
+    scrape(ctx, buildurl(domain))
+end
+
+function buildurl(domain)
+    return "http://archive.is/*." .. domain
+end
