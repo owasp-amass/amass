425705781a2b787e25c65a3f43942b99e6e4ea2f
caffix <caffix@github.com>
2021-02-02 23:44:55 -0500

    Added filtering to the enumeration pipeline input source

--- enum/input.go
+++ enum/input.go
@@ -5,22 +5,28 @@ package enum
 
 import (
 	"context"
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/OWASP/Amass/v3/requests"
+	"github.com/OWASP/Amass/v3/stringfilter"
 	"github.com/caffix/pipeline"
 	"github.com/caffix/queue"
 )
 
 const (
-	minWaitForData = 5 * time.Second
-	maxWaitForData = 30 * time.Second
+	minWaitForData = 30 * time.Second
+	maxWaitForData = 60 * time.Second
 )
 
 // enumSource handles the filtering and release of new Data in the enumeration.
 type enumSource struct {
+	sync.Mutex
 	enum     *Enumeration
 	queue    queue.Queue
+	filter   stringfilter.Filter
+	count    int64
 	done     chan struct{}
 	maxSlots int
 	timeout  time.Duration
@@ -31,6 +37,7 @@ func newEnumSource(e *Enumeration, slots int) *enumSource {
 	r := &enumSource{
 		enum:     e,
 		queue:    queue.NewQueue(),
+		filter:   stringfilter.NewBloomFilter(filterMaxSize),
 		done:     make(chan struct{}),
 		maxSlots: slots,
 		timeout:  minWaitForData,
@@ -55,7 +62,7 @@ func (r *enumSource) InputName(req *requests.DNSRequest) {
 	if req == nil || req.Name == "" {
 		return
 	}
-	if r.enum.Config.IsDomainInScope(req.Name) {
+	if r.accept(req.Name, req.Tag) && r.enum.Config.IsDomainInScope(req.Name) {
 		r.queue.Append(req)
 	}
 }
@@ -68,9 +75,35 @@ func (r *enumSource) InputAddress(req *requests.AddrRequest) {
 	default:
 	}
 
-	if req != nil && req.Address != "" {
+	if req != nil && req.Address != "" && r.accept(req.Address, req.Tag) {
 		r.queue.Append(req)
 	}
+}
+
+func (r *enumSource) accept(s string, tag string) bool {
+	r.Lock()
+	defer r.Unlock()
+
+	// Check if it's time to reset our bloom filter due to number of elements seen
+	if r.count >= filterMaxSize {
+		r.count = 0
+		r.filter = stringfilter.NewBloomFilter(filterMaxSize)
+	}
+
+	trusted := requests.TrustedTag(tag)
+	// Do not submit names from untrusted sources, after already receiving the name
+	// from a trusted source
+	if !trusted && r.filter.Has(s+strconv.FormatBool(true)) {
+		return false
+	}
+	// At most, a FQDN will be accepted from an untrusted source first, and then
+	// reconsidered from a trusted data source
+	if r.filter.Duplicate(s + strconv.FormatBool(trusted)) {
+		return false
+	}
+
+	r.count++
+	return true
 }
 
 // Next implements the pipeline InputSource interface.
