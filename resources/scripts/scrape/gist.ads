3534cf8b00175294b9f5d6c5bd82ddeb9dff08bd
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-07-30 15:04:43 +0700

    Added Github Gist as datasource

---
+++ resources/scripts/scrape/gist.ads
@@ -0,0 +1,33 @@
+-- Copyright 2021 Jeff Foley. All rights reserved.
+-- Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+name = "Gist"
+type = "scrape"
+
+function start()
+    setratelimit(2)
+end
+
+function vertical(ctx, domain)
+    local gistre = "https://gist\\.github\\.com/.(.*)/[a-z0-9]{32}"
+    for i=1,10 do
+        local resp, err = request(ctx, {url=buildurl(domain, i)})
+        if (err ~= nil and err ~= "") then
+            break
+        end
+
+        local gists = find(resp, gistre)
+        if (gists == nil or #gists == 0) then
+            break
+        end
+
+        for _, url in pairs(gists) do
+            scrape(ctx, {['url']=url})
+            checkratelimit()
+        end
+    end
+end
+
+function buildurl(domain, pagenum)
+    return "https://gist.github.com/search?ref=searchresults&q=" .. domain .. "&p=" .. pagenum
+end
