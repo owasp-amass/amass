33aabce32099a521aa97cc6f09d9b0575aa99214
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-08-05 12:52:28 +0700

    Added UKWebArchive as a data source

---
+++ resources/scripts/archive/ukwebarchive.ads
@@ -0,0 +1,17 @@
+-- Copyright 2021 Jeff Foley. All rights reserved.
+-- Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+name = "UKWebArchive"
+type = "archive"
+
+function start()
+    setratelimit(1)
+end
+
+function vertical(ctx, domain)
+    scrape(ctx, {['url']=buildurl(domain)})
+end
+
+function buildurl(domain)
+    return "https://www.webarchive.org.uk/wayback/archive/*?matchType=domain&url=" .. domain
+end
