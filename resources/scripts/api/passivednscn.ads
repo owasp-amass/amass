72516c224ef74059f985f403a38613683996e3eb
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-08-13 18:46:08 +0700

    Added PassiveDNSCN as a data source

---
+++ resources/scripts/api/passivednscn.ads
@@ -0,0 +1,46 @@
+-- Copyright 2021 Jeff Foley. All rights reserved.
+-- Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+name = "PassiveDNSCN"
+type = "api"
+
+function start()
+    set_rate_limit(1)
+end
+
+function check()
+    local c
+    local cfg = datasrc_config()
+    if cfg ~= nil then
+        c = cfg.credentials
+    end
+
+    if (c ~= nil and c.key ~= nil and c.key ~= "") then
+        return true
+    end
+    return false
+end
+
+function vertical(ctx, domain)
+    local c
+    local cfg = datasrc_config()
+    if cfg ~= nil then
+        c = cfg.credentials
+    end
+
+    if (c == nil or c.key == nil or c.key == "") then
+        return
+    end
+
+    scrape(ctx, {
+        url=api_url(domain),
+        headers={
+            ['Accept']="application/json",
+            ['X-Authtoken']=c.key,
+        },
+    })
+end
+
+function api_url(domain)
+    return "https://api.passivedns.cn/flint/rrset/*." .. domain .. "/?source=ALL&batch=1000"
+end
