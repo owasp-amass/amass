573e62ccc47251bd07a7559c683ef63e79ecea49
Pham Sy Minh <59408894+shelld3v@users.noreply.github.com>
2021-08-18 11:32:52 +0700

    Gists: Increase the number of pages to scrape

--- resources/scripts/scrape/gists.ads
+++ resources/scripts/scrape/gists.ads
@@ -12,7 +12,7 @@ end
 
 function vertical(ctx, domain)
     local gist_re = "https://gist[.]github[.]com/[a-zA-Z0-9-]{1,39}/[a-z0-9]{32}"
-    for i=1,10 do
+    for i=1,20 do
         local resp, err = request(ctx, {url=build_url(domain, i)})
         if (err ~= nil and err ~= "") then
             break
@@ -23,8 +23,8 @@ function vertical(ctx, domain)
             break
         end
 
-        for i, url in pairs(gists) do
-            scrape(ctx, {['url']=url})
+        for _, gist in pairs(gists) do
+            scrape(ctx, {url=gist})
         end
         check_rate_limit()
     end
@@ -32,9 +32,9 @@ end
 
 function build_url(domain, pagenum)
     local params = {
-        ref="searchresults",
-        q=domain,
-        p=pagenum,
+        ['ref']="searchresults",
+        ['q']=domain,
+        ['p']=pagenum,
     }
     return "https://gist.github.com/search?" .. url.build_query_string(params)
 end
