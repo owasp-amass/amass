fadfa69f9b7703bf999dda82947d15aa357e9eeb
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-08-22 00:18:30 +0700

    Added Ahrefs and Quake as data sources

---
+++ resources/scripts/api/ahrefs.ads
@@ -0,0 +1,65 @@
+-- Copyright 2021 Jeff Foley. All rights reserved.
+-- Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+local url = require("url")
+local json = require("json")
+
+name = "Ahrefs"
+type = "api"
+
+function start()
+    set_rate_limit(1)
+end
+
+function check()
+    local c
+    local cfg = datasrc_config()
+    if cfg ~= nil then
+        c = cfg.credentials
+    end
+
+    if (c ~= nil and c.key ~= nil and c.key ~= "") then
+        return true
+    end
+    return false
+end
+
+function vertical(ctx, domain)
+    local c
+    local cfg = datasrc_config()
+    if cfg ~= nil then
+        c = cfg.credentials
+    end
+
+    if (c == nil or c.key == nil or c.key == "") then
+        return
+    end
+
+    local resp, err = request(ctx, {url=build_url(domain, c.key)})
+    if (err ~= nil and err ~= "") then
+        return
+    end
+
+    local d = json.decode(resp)
+    if (d == nil or d.refpages == nil or #d.refpages == 0) then
+        return
+    end
+
+    for _, r in pairs(d.refpages) do
+        send_names(ctx, r.url_to)
+    end
+end
+
+function build_url(domain, key)
+    local params = {
+        ['target']=domain,
+        ['token']=key,
+        ['from']="backlinks",
+        ['mode']="subdomains",
+        ['limit']="1000",
+        ['order_by']="first_seen%3Adesc",
+        ['output']="json",
+    }
+
+    return "https://apiv2.ahrefs.com/?" .. url.build_query_string(params)
+end
---
+++ resources/scripts/api/quake.ads
@@ -0,0 +1,70 @@
+-- Copyright 2021 Jeff Foley. All rights reserved.
+-- Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+name = "Quake"
+type = "api"
+
+function start()
+    set_rate_limit(1)
+end
+
+function check()
+    local c
+    local cfg = datasrc_config()
+    if cfg ~= nil then
+        c = cfg.credentials
+    end
+
+    if (c ~= nil and c.key ~= nil and c.key ~= "") then
+        return true
+    end
+    return false
+end
+
+function vertical(ctx, domain)
+    local c
+    local cfg = datasrc_config()
+    if cfg ~= nil then
+        c = cfg.credentials
+    end
+
+    if (c == nil or c.key == nil or c.key == "") then
+        return
+    end
+
+    p = 0
+    while(true) do
+        local body = json.encode({
+            ['query']="domain:*." .. domain,
+            ['start']=p,
+            ['size']=1000,
+        })
+
+        local resp, err = request(ctx, {
+            url="https://quake.360.cn/api/v3/search/quake_service",
+            method="POST",
+            data=body,
+            headers={
+                ['Content-Type']="application/json",
+                ['X-QuakeToken']=c.key,
+            }
+        })
+        if (err ~= nil and err ~= "") then
+            return
+        end
+
+        local j = json.decode(resp)
+        if (j == nil or j.code ~= 0 or j['meta'].pagination.total == 0) then
+            return
+        end
+
+        for _, d in pairs(j.data) do
+            new_name(ctx, d['service'].http.host)
+        end
+
+        if j['meta'].pagination.total < 1000 then
+            break
+        end
+        p += 1000
+    end
+end
