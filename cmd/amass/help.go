0bb0bd712d9c7f1a09b1775b0cef39190156033f
Mindslave <mindslave.blog@protonmail.com>
2021-10-11 10:52:27 +0200

    Add help subcommand

---
+++ cmd/amass/help.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+)
+
+
+func runHelpCommand(clArgs []string) {
+	help := []string{"-help"}
+	helpBuf := new(bytes.Buffer)
+	helpCommand := flag.NewFlagSet("db", flag.ContinueOnError)
+	helpCommand.SetOutput(helpBuf)
+	if len(clArgs) < 1 {
+		commandUsage(mainUsageMsg, helpCommand, helpBuf)
+		return 
+	}
+	switch clArgs[0] {
+	case "db":
+		runDBCommand(help)
+	case "dns":
+		runDNSCommand(help)
+	case "enum":
+		runEnumCommand(help)
+	case "intel":
+		runIntelCommand(help)
+	case "track":
+		runTrackCommand(help)
+	case "viz":
+		runVizCommand(help)
+	default:
+		commandUsage(mainUsageMsg, helpCommand, helpBuf)
+		return	
+	}
+}
