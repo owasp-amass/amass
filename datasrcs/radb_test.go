37f2841e6faffc56cb2675ee22927a75ac2f1563
FORCHA <forchapearl1@gmail.com>
2022-02-16 10:15:07 +0100

    Update radb_test.go
    -Shutdown System Object
    -Removed unecessary spacing

--- datasrcs/radb_test.go
+++ datasrcs/radb_test.go
@@ -1,37 +1,41 @@
 package datasrcs
+
 import (
 	"reflect"
 	"testing"
+
 	"github.com/OWASP/Amass/v3/config"
 	"github.com/OWASP/Amass/v3/systems"
 )
 
 func TestRegistryRADbURL(t *testing.T) {
+
 	tests := []struct {
-		name    string
+		name     string
 		input    string
 		expected string
 	}{
-		{	name:     "Case arin",
-			input:     "arin",
+		{
+			name:     "Case arin",
+			input:    "arin",
 			expected: "https://rdap.arin.net/registry/",
-		},
-		{	name:     "Case ripencc",
-			input:     "ripencc",
+		}, {
+			name:     "Case ripencc",
+			input:    "ripencc",
 			expected: "https://rdap.db.ripe.net/",
-		},
-		{	name:     "Case apnic",
-			input:     "apnic",
+		}, {
+			name:     "Case apnic",
+			input:    "apnic",
 			expected: "https://rdap.apnic.net/",
-		},
-		{	name:     "Case lacnic",
-			input:     "lacnic",
+		}, {
+			name:     "Case lacnic",
+			input:    "lacnic",
 			expected: "https://rdap.lacnic.net/rdap/",
-		},
-		{	name:     "Case afrinic",
-			input:     "afrinic",
+		}, {
+			name:     "Case afrinic",
+			input:    "afrinic",
 			expected: "https://rdap.afrinic.net/rdap/",
-		},								
+		},
 	}
 	cfg := config.NewConfig()
 	sys, err := systems.NewLocalSystem(cfg)
@@ -39,7 +43,7 @@ func TestRegistryRADbURL(t *testing.T) {
 		return
 	}
 	myRADb := NewRADb(sys)
-		for _, tt := range tests {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			result := myRADb.registryRADbURL(tt.input)
 			if !reflect.DeepEqual(result, tt.expected) {
@@ -47,4 +51,5 @@ func TestRegistryRADbURL(t *testing.T) {
 			}
 		})
 	}
+	defer sys.Shutdown()
 }
