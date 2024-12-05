0415b95a87400f2752ca36bfb32ff70f6db66f78
caffix <caffix@users.noreply.github.com>
2024-12-05 12:52:24 -0500

    performance improvement

--- engine/plugins/ip_netblock.go
+++ engine/plugins/ip_netblock.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net/netip"
+	"sync"
 	"time"
 
 	"github.com/owasp-amass/amass/v4/engine/plugins/support"
@@ -21,13 +22,28 @@ import (
 	"github.com/owasp-amass/open-asset-model/relation"
 )
 
+type sessnets struct {
+	last time.Time
+	nets map[string]*oamnet.Netblock
+}
+
 type ipNetblock struct {
-	name string
-	log  *slog.Logger
+	name      string
+	log       *slog.Logger
+	done      chan struct{}
+	mlock     sync.Mutex
+	netblocks map[string]*sessnets
 }
 
 func NewIPNetblock() et.Plugin {
-	return &ipNetblock{name: "IP-Netblock"}
+	p := &ipNetblock{
+		name:      "IP-Netblock",
+		done:      make(chan struct{}, 2),
+		netblocks: make(map[string]*sessnets),
+	}
+
+	go p.checkNetblocks()
+	return p
 }
 
 func (d *ipNetblock) Name() string {
@@ -55,11 +71,10 @@ func (d *ipNetblock) Start(r et.Registry) error {
 }
 
 func (d *ipNetblock) Stop() {
+	close(d.done)
 	d.log.Info("Plugin stopped")
 }
 
-// ipLookup function queries the bgptools whois server using an
-// IP address to retrieve related ASN, netblock, and RIR details.
 func (d *ipNetblock) lookup(e *et.Event) error {
 	ip, ok := e.Entity.Asset.(*oamnet.IPAddress)
 	if !ok {
@@ -85,9 +100,13 @@ func (d *ipNetblock) lookup(e *et.Event) error {
 	} else {
 		var err error
 
-		netblock, err = support.IPToNetblockWithAttempts(e.Session, ip, 60, time.Second)
+		netblock, err = d.lookupNetblock(e.Session.ID().String(), ip)
 		if err != nil {
-			return nil
+			netblock, err = support.IPToNetblockWithAttempts(e.Session, ip, 60, time.Second)
+			if err != nil {
+				return nil
+			}
+			d.addNetblock(e.Session.ID().String(), netblock)
 		}
 	}
 
@@ -107,9 +126,8 @@ func (d *ipNetblock) lookup(e *et.Event) error {
 }
 
 func (d *ipNetblock) reservedAS(e *et.Event, netblock *oamnet.Netblock) {
-	group := slog.Group("plugin", "name", d.name, "handler", d.name+"-Handler")
-
 	asn, err := e.Session.Cache().CreateAsset(&oamnet.AutonomousSystem{Number: 0})
+
 	if err == nil && asn != nil {
 		autnum, err := e.Session.Cache().CreateAsset(&oamreg.AutnumRecord{
 			Number: 0,
@@ -123,9 +141,6 @@ func (d *ipNetblock) reservedAS(e *et.Event, netblock *oamnet.Netblock) {
 				FromEntity: asn,
 				ToEntity:   autnum,
 			})
-
-			e.Session.Log().Info("relationship discovered", "from", 0, "relation",
-				"registration", "to", "Reserved Network Address Blocks", group)
 		}
 
 		if nb, err := e.Session.Cache().CreateAsset(netblock); err == nil && nb != nil {
@@ -134,9 +149,75 @@ func (d *ipNetblock) reservedAS(e *et.Event, netblock *oamnet.Netblock) {
 				FromEntity: asn,
 				ToEntity:   nb,
 			})
-
-			e.Session.Log().Info("relationship discovered", "from", 0,
-				"relation", "announces", "to", netblock.CIDR.String(), group)
 		}
+	}
+}
+
+func (d *ipNetblock) lookupNetblock(sessid string, ip *oamnet.IPAddress) (*oamnet.Netblock, error) {
+	d.mlock.Lock()
+	defer d.mlock.Unlock()
+
+	n, ok := d.netblocks[sessid]
+	if !ok {
+		return nil, errors.New("no netblocks found")
+	}
+	n.last = time.Now()
+
+	var size int
+	var found *oamnet.Netblock
+	for _, nb := range n.nets {
+		if nb.CIDR.Contains(ip.Address) {
+			if s := nb.CIDR.Masked().Bits(); s > size {
+				size = s
+				found = nb
+			}
+		}
+	}
+
+	if found == nil {
+		return nil, errors.New("no netblock match")
+	}
+	return found, nil
+}
+
+func (d *ipNetblock) addNetblock(sessid string, nb *oamnet.Netblock) {
+	d.mlock.Lock()
+	defer d.mlock.Unlock()
+
+	if _, found := d.netblocks[sessid]; !found {
+		d.netblocks[sessid] = &sessnets{nets: make(map[string]*oamnet.Netblock)}
+	}
+
+	d.netblocks[sessid].last = time.Now()
+	d.netblocks[sessid].nets[nb.CIDR.String()] = nb
+}
+
+func (d *ipNetblock) checkNetblocks() {
+	t := time.NewTicker(10 * time.Minute)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-d.done:
+			return
+		case <-t.C:
+			d.cleanSessionNetblocks()
+		}
+	}
+}
+
+func (d *ipNetblock) cleanSessionNetblocks() {
+	d.mlock.Lock()
+	defer d.mlock.Unlock()
+
+	var sessids []string
+	for sessid, n := range d.netblocks {
+		if time.Since(n.last) > time.Hour {
+			sessids = append(sessids, sessid)
+		}
+	}
+
+	for _, sessid := range sessids {
+		delete(d.netblocks, sessid)
 	}
 }
