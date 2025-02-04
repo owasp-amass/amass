8051ff6272bbc9277ee0a2792c9fbc3984a8b81c
caffix <caffix@users.noreply.github.com>
2025-02-03 22:32:14 -0500

    starting changes to embedded files

--- resources/resources.go
+++ resources/resources.go
@@ -1,4 +1,4 @@
-// Copyright © by Jeff Foley 2017-2024. All rights reserved.
+// Copyright © by Jeff Foley 2017-2025. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 // SPDX-License-Identifier: Apache-2.0
 
@@ -7,17 +7,19 @@ package resources
 import (
 	"embed"
 	"fmt"
-	"io"
+	"io/fs"
 )
 
 //go:embed alterations.txt namelist.txt
 var resourceFS embed.FS
 
-func GetResourceFile(path string) (io.Reader, error) {
+func GetResourceFile(path string) (fs.File, error) {
 	file, err := resourceFS.Open(path)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to obtain the embedded file: %s: %v", path, err)
 	}
+
 	return file, err
 }
 
