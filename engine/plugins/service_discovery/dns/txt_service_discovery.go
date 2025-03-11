86a29a25954524805bb14786b064e1d6e7928660
Rishi <rishi@rxerium.com>
2025-03-11 08:02:47 +0000

    Uses the entity tags to get TXT records from the cache

--- engine/plugins/service_discovery/dns/txt_service_discovery.go
+++ engine/plugins/service_discovery/dns/txt_service_discovery.go
@@ -6,7 +6,7 @@ import (
     "time"
 
     et "github.com/owasp-amass/amass/v4/engine/types"
-    dnsPlugins "github.com/owasp-amass/amass/v4/engine/plugins/dns"
+    oamdns "github.com/owasp-amass/open-asset-model/dns"
 )
 
 type txtServiceDiscovery struct {
@@ -17,6 +17,10 @@ type txtServiceDiscovery struct {
 func NewTXTServiceDiscovery() et.Plugin {
     return &txtServiceDiscovery{
         name: "txt_service_discovery",
+        source: &et.Source{
+            Name:       "txt_service_discovery",
+            Confidence: 100,
+        },
     }
 }
 
@@ -31,38 +35,49 @@ func (t *txtServiceDiscovery) Start(r et.Registry) error {
 func (t *txtServiceDiscovery) Stop() {}
 
 func (t *txtServiceDiscovery) check(e *et.Event) error {
-    since := time.Time{}
-
-    // Use the exported type or constructor from the dns plugin
-    txtPlugin := &dnsPlugins.dnsTXT{
-        Name:   "dnsTXT",
-        Source: t.source,
+    since := time.Time{} // Get all TXT records
+    
+    // Get entity tags that contain TXT records
+    tags, err := e.Session.Cache().GetEntityTags(e.Entity, since, "dns_record")
+    if err != nil {
+        return err
     }
-
-    // Use the dnsTXT lookup method to fetch TXT records
-    txtRecords := txtPlugin.Lookup(e, e.Entity, since)
-
+    
     matchers := map[string]string{
         "google-site-verification":        "Google",
         "status-page-domain-verification": "StatusPage Domain",
         "facebook-domain-verification=":   "Facebook",
     }
-
+    
     var foundName string
-    for _, rec := range txtRecords {
-        for pattern, serviceName := range matchers {
-            if strings.Contains(rec.Data, pattern) {
-                foundName = serviceName
-                break
+    // Process each tag to find TXT records
+    for _, tag := range tags {
+        if prop, ok := tag.Property.(*oamdns.DNSRecordProperty); ok {
+            // Check if it's a TXT record (type 16)
+            if prop.Header.RRType == 16 {
+                // Check for service patterns in the TXT record data
+                for pattern, serviceName := range matchers {
+                    if strings.Contains(prop.Data, pattern) {
+                        foundName = serviceName
+                        break
+                    }
+                }
+                if foundName != "" {
+                    break
+                }
             }
         }
-        if foundName != "" {
-            break
-        }
     }
-
+    
     if foundName != "" {
-        slog.Info("Discovered "+foundName+" service in TXT record", "entity", e.Entity)
+        fqdn, _ := e.Entity.Asset.(*oamdns.FQDN)
+        domainName := "unknown"
+        if fqdn != nil {
+            domainName = fqdn.Name
+        }
+        slog.Info("Discovered "+foundName+" service in TXT record", 
+            "domain", domainName,
+            "plugin", t.name)
     }
     return nil
 }
