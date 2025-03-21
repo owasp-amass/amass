4303d9b27c0b79462aeec774c9d3ea7bb028b6b5
caffix <caffix@users.noreply.github.com>
2025-03-21 12:46:55 -0400

    additional performance enhancements

--- engine/plugins/support/resolvers.go
+++ engine/plugins/support/resolvers.go
@@ -73,7 +73,7 @@ func dnsQuery(msg *dns.Msg, r *pool.Pool, attempts int) (*dns.Msg, error) {
 }
 
 func trustedResolvers() *pool.Pool {
-	timeout := 5 * time.Second
+	timeout := time.Second
 	cpus := runtime.NumCPU()
 	// wildcard detector
 	serv := servers.NewNameserver("8.8.8.8", timeout)
