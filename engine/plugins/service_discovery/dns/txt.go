fd02aed4fafb8fe4e23b409219b157e1753af77b
Rishi <rishi@rxerium.com>
2025-06-07 14:13:40 +0100

    fix

--- engine/plugins/service_discovery/dns/txt.go
+++ engine/plugins/service_discovery/dns/txt.go
@@ -425,3 +425,64 @@ func (t *txtServiceDiscovery) retrieveEntityTags(e *et.Event, fqdn *oamdns.FQDN,
 
     return props
 }
+
+// txtPluginManager wraps the TXT service discovery plugin lifecycle.
+type txtPluginManager struct {
+    name     string
+    log      *slog.Logger
+    source   *et.Source
+    discover *txtServiceDiscovery // Corrected the struct name
+}
+
+// NewTXTPlugin returns a new instance of the TXT service discovery plugin.
+func NewTXTPlugin() et.Plugin {
+    return &txtPluginManager{
+        name: "txt_service_discovery",
+        source: &et.Source{
+            Name:       "txt_service_discovery",
+            Confidence: 100,
+        },
+    }
+}
+
+// NewDNSPlugin is provided for backward compatibility with the plugin loader.
+// It simply returns the TXT plugin under the original DNS-plugin factory name.
+func NewDNSPlugin() et.Plugin {
+    return NewTXTPlugin()
+}
+
+// Name returns the plugin's name.
+func (tpm *txtPluginManager) Name() string {
+    return tpm.name
+}
+
+// Start registers the plugin handler with the registry.
+func (tpm *txtPluginManager) Start(r et.Registry) error {
+    tpm.log = r.Log().WithGroup("plugin").With("name", tpm.name)
+
+    tpm.discover = &txtServiceDiscovery{ // Corrected the struct name
+        name:   tpm.name + "-FQDN-Check",
+        source: tpm.source,
+    }
+
+    err := r.RegisterHandler(&et.Handler{
+        Plugin:     tpm,
+        Name:       tpm.discover.name,
+        Priority:   9,
+        Transforms: []string{"DNSRecord"},
+        EventType:  (oamdns.FQDN{}).AssetType(),
+        Callback:   tpm.discover.check,
+    })
+    if err != nil {
+        tpm.log.Error("Failed to register handler", "error", err)
+        return err
+    }
+
+    tpm.log.Info("Plugin started")
+    return nil
+}
+
+// Stop logs that the plugin has stopped.
+func (tpm *txtPluginManager) Stop() {
+    tpm.log.Info("Plugin stopped")
+}
