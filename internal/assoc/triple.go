c805063e6c18ec74b54ee67bd43cb7c11135e107
caffix <caffix@users.noreply.github.com>
2025-06-15 22:25:25 -0400

    continued the triple parsing development

--- internal/assoc/triple.go
+++ internal/assoc/triple.go
@@ -5,10 +5,24 @@
 package assoc
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
 	oam "github.com/owasp-amass/open-asset-model"
 )
 
+const (
+	DirectionIncoming = iota
+	DirectionOutgoing
+)
+
+var (
+	tripleFields = []string{"subject", "predicate", "object"}
+)
+
 type Triple struct {
+	Direction int // 0 for incoming, 1 for outgoing
 	Subject   string
 	Predicate string
 	Object    string
@@ -17,6 +31,7 @@ type Triple struct {
 type Node struct {
 	Key        string
 	Type       oam.AssetType
+	Since      time.Time
 	Attributes map[string]string
 	Properties []Property
 }
@@ -24,6 +39,7 @@ type Node struct {
 type Predicate struct {
 	Label      string
 	Type       oam.RelationType
+	Since      time.Time
 	Attributes map[string]string
 	Properties []Property
 }
@@ -31,6 +47,75 @@ type Predicate struct {
 type Property struct {
 	Name       string
 	Value      string
+	Since      time.Time
 	Type       oam.PropertyType
 	Attributes map[string]string
+}
+
+func ParseTriple(triple string) (*Triple, error) {
+	tristrs, direction, err := splitTriple(triple)
+	if err != nil {
+		return nil, fmt.Errorf("invalid triple format: %w", err)
+	}
+
+	return &Triple{
+		Direction: direction,
+		Subject:   parts[0],
+		Predicate: parts[1],
+		Object:    parts[2],
+	}, nil
+}
+
+func splitTriple(triple string) ([]string, int, error) {
+	start := 0
+	var tstrs []string
+	direction := DirectionOutgoing
+
+	for i := 0; i < 3; i++ {
+		substr := triple[start:]
+
+		sidx := strings.Index(substr, "<")
+		if sidx == -1 {
+			return nil, direction, fmt.Errorf("triple must contain an opening angle bracket for the %s", tripleFields[i])
+		}
+		sidx += 1 // Move past the opening angle bracket
+
+		eidx := strings.Index(substr, ">")
+		if eidx == -1 {
+			return nil, direction, fmt.Errorf("triple must contain a closing angle bracket for the %s", tripleFields[i])
+		}
+		if eidx <= sidx {
+			return nil, direction, fmt.Errorf("the %s must contain a closing angle bracket after an opening angle bracket", tripleFields[i])
+		}
+		tstrs = append(tstrs, strings.TrimSpace(triple[sidx:eidx]))
+
+		start += eidx + 1 // Move past the closing angle bracket
+		substr = triple[start:]
+		if i == 0 {
+			if idx := strings.Index(substr, "<-"); idx != -1 && (idx == 0 || idx == 1) {
+				direction = DirectionIncoming
+				start += 2 // Move past the "<-"
+			} else if idx := strings.Index(substr, "-"); idx != -1 && (idx == 0 || idx == 1) {
+				start += 1 // Move past the "-"
+			} else {
+				return nil, direction, fmt.Errorf("triple must contain a hyphen or '<-' after the subject")
+			}
+		} else if i == 1 {
+			if idx := strings.Index(substr, "->"); idx != -1 && (idx == 0 || idx == 1) {
+				if direction == DirectionIncoming {
+					return nil, direction, fmt.Errorf("triple cannot have both '<-' and '->'")
+				}
+				start += 2 // Move past the "<-"
+			} else if idx := strings.Index(substr, "-"); idx != -1 && (idx == 0 || idx == 1) {
+				if direction == DirectionOutgoing {
+					return nil, direction, fmt.Errorf("triple must have a direction specified with '<-' or '->'")
+				}
+				start += 1 // Move past the "-"
+			} else {
+				return nil, direction, fmt.Errorf("triple must contain a hyphen or '<-' after the predicate")
+			}
+		}
+	}
+
+	return tstrs, direction, nil
 }
