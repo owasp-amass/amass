68d63b3797ac854b98b6eddfdfd0e2bea13a2736
caffix <caffix@users.noreply.github.com>
2025-06-26 21:06:20 -0400

    fixes

--- internal/assoc/cli.go
+++ internal/assoc/cli.go
@@ -10,15 +10,11 @@ import (
 	"flag"
 	"io"
 	"os"
-	"time"
 
 	"github.com/fatih/color"
 	"github.com/owasp-amass/amass/v4/config"
 	"github.com/owasp-amass/amass/v4/internal/afmt"
 	"github.com/owasp-amass/amass/v4/internal/tools"
-	assetdb "github.com/owasp-amass/asset-db"
-	"github.com/owasp-amass/asset-db/cache"
-	"github.com/owasp-amass/asset-db/repository/sqlrepo"
 	"github.com/owasp-amass/asset-db/triples"
 )
 
@@ -142,19 +138,6 @@ func CLIWorkflow(cmdName string, clArgs []string) {
 		_, _ = afmt.R.Fprintln(color.Error, "Failed to connect with the database")
 		os.Exit(1)
 	}
-	// create a new in-memory SQLite database for performance
-	cdb, err := assetdb.New(sqlrepo.SQLiteMemory, "")
-	if err != nil {
-		_, _ = afmt.R.Fprintf(color.Error, "Failed to create an in-memory SQLite database: %v\n", err)
-		os.Exit(1)
-	}
-	// Create a cache for the database to speed up the association walk
-	c, err := cache.New(cdb, db, time.Minute)
-	if err != nil {
-		_, _ = afmt.R.Fprintf(color.Error, "Failed to create a cache for the database: %v\n", err)
-		os.Exit(1)
-	}
-	defer func() { _ = c.Close() }()
 
 	var tris []*triples.Triple
 	for _, tstr := range args.Triples {
@@ -174,7 +157,7 @@ func CLIWorkflow(cmdName string, clArgs []string) {
 		os.Exit(1)
 	}
 
-	results, err := triples.Extract(c, tris)
+	results, err := triples.Extract(db, tris)
 	if err != nil {
 		_, _ = afmt.R.Fprintf(color.Error, "Failed to extract associations: %v\n", err)
 		os.Exit(1)
