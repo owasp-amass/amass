1a498a579e32293af28ddc5156a681445eb7d705
Rishi <rishi@rxerium.com>
2025-07-14 21:08:12 +0100

    v5 updates

--- engine/plugins/service_discovery/dns/plugin.go
+++ engine/plugins/service_discovery/dns/plugin.go
@@ -3,7 +3,7 @@ package dns
 import (
 	"log/slog"
 
-	et "github.com/owasp-amass/amass/v4/engine/types"
+	et "github.com/owasp-amass/amass/v5/engine/types"
 	oam "github.com/owasp-amass/open-asset-model"
 )
 
