61f53cc91ff38afd8d227c1e255e5e0b38df9922
caffix <caffix@users.noreply.github.com>
2025-09-04 17:03:58 -0400

    fix for a linter warning

--- internal/assoc/cli.go
+++ internal/assoc/cli.go
@@ -181,7 +181,7 @@ func readTriplesTextFromFile(filePath string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
+	defer func() { _ = file.Close() }()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
